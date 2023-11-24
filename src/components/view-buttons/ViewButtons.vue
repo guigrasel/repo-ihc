b46c718ed86a1b39608c13b1953c500e60489bbe
Guilherme Grasel <guigrasel@gmail.com>
2023-11-24 12:40:42 -0300

    ✨ Função get para os lembretes

--- src/components/view-buttons/ViewButtons.vue
+++ src/components/view-buttons/ViewButtons.vue
@@ -29,57 +29,7 @@ const props = defineProps({
     type: String,
     required: true
   },
-  lembretes: {
-    type: Array as () => Lembrete[],
-    default: () => []
-  }
 })
-
-// import { DateTime } from "luxon";
-
-interface Lembrete {
-  data: string;
-  title: string;
-  descricao: string;
-}
-
-// interface LembretesSeparados {
-//   hoje: Lembrete[];
-//   pendentes: Lembrete[];
-//   concluidos: Lembrete[];
-// }
-
-// function separarLembretes(lembretes: Lembrete[]): LembretesSeparados {
-//   const hoje: Lembrete[] = [];
-//   const pendentes: Lembrete[] = [];
-//   const concluidos: Lembrete[] = [];
-
-//   const dataAtual: DateTime = DateTime.now();
-
-//   lembretes.forEach((lembrete: Lembrete) => {
-//     const dataLembrete: DateTime = DateTime.fromISO(lembrete.data);
-
-//     if (dataLembrete.hasSame(dataAtual, 'day')) {
-//       hoje.push(lembrete);
-//       return;
-//     }
-
-//     if (dataLembrete < dataAtual) {
-//       concluidos.push(lembrete);
-//       return;
-//     }
-
-//     pendentes.push(lembrete);
-//   });
-
-//   return {
-//     hoje,
-//     pendentes,
-//     concluidos
-//   };
-// }
-
-// const { hoje, pendentes, concluidos } = separarLembretes(props.lembretes);
 </script>
 
 <style scoped>
--- src/stores/lembretes.ts
+++ src/stores/lembretes.ts
@@ -1,4 +1,6 @@
 import { defineStore } from "pinia";
+import { DateTime } from "luxon";
+
 import { FormLembrete } from "../components/ModalAddLembrete.vue";
 
 interface Lembretes {
@@ -6,6 +8,18 @@ interface Lembretes {
   carro: FormLembrete[];
   eletrodomesticos: FormLembrete[];
   outros: FormLembrete[];
+}
+
+interface Lembrete {
+  data: string;
+  title: string;
+  descricao: string;
+}
+
+interface LembretesSeparados {
+  hoje: Lembrete[];
+  pendentes: Lembrete[];
+  concluidos: Lembrete[];
 }
 
 export const useLembretesStore = defineStore("lembretes", () => {
@@ -32,9 +46,45 @@ export const useLembretesStore = defineStore("lembretes", () => {
 
     notifyPositive("Lembrete adicionado com sucesso!");
   }
+  
+  function separarLembretes(lembretes: Lembrete[]): LembretesSeparados {
+    const hoje: Lembrete[] = [];
+    const pendentes: Lembrete[] = [];
+    const concluidos: Lembrete[] = [];
+  
+    const dataAtual: DateTime = DateTime.now();
+  
+    lembretes.forEach((lembrete: Lembrete) => {
+      const dataLembrete: DateTime = DateTime.fromISO(lembrete.data);
+  
+      if (dataLembrete.hasSame(dataAtual, 'day')) {
+        hoje.push(lembrete);
+        return;
+      }
+  
+      if (dataLembrete < dataAtual) {
+        concluidos.push(lembrete);
+        return;
+      }
+  
+      pendentes.push(lembrete);
+    });
+  
+    return {
+      hoje,
+      pendentes,
+      concluidos,
+    };
+  }
+
+
+  function getLembretes(tipo: keyof Lembretes) {
+    return separarLembretes(lembretes[tipo])
+  }
 
   return {
     lembretes,
     novoLembrete,
+    getLembretes
   };
 });
