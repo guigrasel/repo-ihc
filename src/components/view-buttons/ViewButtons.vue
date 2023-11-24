b32a30a292c56448763c0efdf27a203a5ee694d9
Guilherme Grasel <guigrasel@gmail.com>
2023-11-23 21:12:19 -0300

    🚧 Comenta código para buildar

--- src/components/view-buttons/ViewButtons.vue
+++ src/components/view-buttons/ViewButtons.vue
@@ -35,7 +35,7 @@ const props = defineProps({
   }
 })
 
-import { DateTime } from "luxon";
+// import { DateTime } from "luxon";
 
 interface Lembrete {
   data: string;
@@ -43,43 +43,43 @@ interface Lembrete {
   descricao: string;
 }
 
-interface LembretesSeparados {
-  hoje: Lembrete[];
-  pendentes: Lembrete[];
-  concluidos: Lembrete[];
-}
+// interface LembretesSeparados {
+//   hoje: Lembrete[];
+//   pendentes: Lembrete[];
+//   concluidos: Lembrete[];
+// }
 
-function separarLembretes(lembretes: Lembrete[]): LembretesSeparados {
-  const hoje: Lembrete[] = [];
-  const pendentes: Lembrete[] = [];
-  const concluidos: Lembrete[] = [];
+// function separarLembretes(lembretes: Lembrete[]): LembretesSeparados {
+//   const hoje: Lembrete[] = [];
+//   const pendentes: Lembrete[] = [];
+//   const concluidos: Lembrete[] = [];
 
-  const dataAtual: DateTime = DateTime.now();
+//   const dataAtual: DateTime = DateTime.now();
 
-  lembretes.forEach((lembrete: Lembrete) => {
-    const dataLembrete: DateTime = DateTime.fromISO(lembrete.data);
+//   lembretes.forEach((lembrete: Lembrete) => {
+//     const dataLembrete: DateTime = DateTime.fromISO(lembrete.data);
 
-    if (dataLembrete.hasSame(dataAtual, 'day')) {
-      hoje.push(lembrete);
-      return;
-    }
+//     if (dataLembrete.hasSame(dataAtual, 'day')) {
+//       hoje.push(lembrete);
+//       return;
+//     }
 
-    if (dataLembrete < dataAtual) {
-      concluidos.push(lembrete);
-      return;
-    }
+//     if (dataLembrete < dataAtual) {
+//       concluidos.push(lembrete);
+//       return;
+//     }
 
-    pendentes.push(lembrete);
-  });
+//     pendentes.push(lembrete);
+//   });
 
-  return {
-    hoje,
-    pendentes,
-    concluidos
-  };
-}
+//   return {
+//     hoje,
+//     pendentes,
+//     concluidos
+//   };
+// }
 
-const { hoje, pendentes, concluidos } = separarLembretes(props.lembretes);
+// const { hoje, pendentes, concluidos } = separarLembretes(props.lembretes);
 </script>
 
 <style scoped>
