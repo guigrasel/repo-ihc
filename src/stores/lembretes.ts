b292ff5d3ee068353ef5ef7d333b765d2da3c444
Guilherme Grasel <guigrasel@gmail.com>
2023-11-27 22:39:45 -0300

    🔥 Remove reload da tela

--- src/stores/lembretes.ts
+++ src/stores/lembretes.ts
@@ -46,10 +46,6 @@ export const useLembretesStore = defineStore("lembretes", () => {
     salvarLembretes();
 
     notifyPositive("Lembrete adicionado com sucesso!");
-
-    setTimeout(() => {
-      location.reload()
-    }, 700)
   }
   
   function separarLembretes(lembretes: Lembrete[]): LembretesSeparados {
